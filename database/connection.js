0b8cd8e456c2f2f9ff1e8bda7c35148c5e431718
amilin <moh.amilin@moladin.com>
2023-03-15 15:18:35 +0700

    commet console

--- database/connection.js
+++ database/connection.js
@@ -5,7 +5,7 @@ module.exports = class Connection {
   async connect() {
     try {
       await database.sequelize.authenticate();
-      console.log({listModels: database.sequelize.models});
+      // console.log({listModels: database.sequelize.models});
       logger.info('Connection has been established successfully.');
     } catch (error) {
       logger.error('Unable to connect to the database:', error);
